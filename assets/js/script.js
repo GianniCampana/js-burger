3d05b0e67f152ee105cb2b221b21616613c04177
GianniCampana <campanagianni97@gmail.com>
2021-04-19 18:32:26 +0200

    burger termine

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ var coupon = document.getElementById('coupon');
 var defaultPrice = 50;
 var coupons = ['sconto2021', 'sconto-bool'];
 var discount = 0.2;
+
 writePrice(defaultPrice, priceEl);
 
 
@@ -15,36 +16,52 @@ writePrice(defaultPrice, priceEl);
 
 button.addEventListener('click', function(){
     /* controllo nome */
-    var nomeInserito = nameUser.value.trim();
+     var nomeInserito = nameUser.value.trim();
 
       if(nomeInserito.length === 0){
           alert("inserisci il nome");
       }else{ 
-          /* somma elementi */
-          var priceIngredient = 0;
-          for(var i=0; i< ingredients.length; i++){
-              var ingredient = ingredients[i];
-              if(ingredient.checked === true){
-                  priceIngredient += parseInt(ingredient.value);
-              }
-          }
+         
+        stampaprezzo(defaultPrice);
           
-          var totalPrice = defaultPrice + priceIngredient;
-
-          if(coupons.includes(coupon.value) === true){
-                totalPrice -= totalPrice * discount;
-          }
-
-          writePrice(totalPrice, priceEl);
 
       }
     
 });
 
 
+function stampaprezzo(value){
+    var prezzoTot = calcoloPrezzo(value);
+    var cou_pon = coupon.value;
+    if(coupons.includes(cou_pon)){
+        prezzoTot = dis_count(prezzoTot, discount)
+    }
+    writePrice(prezzoTot, priceEl);
+}
+
+function dis_count(prezzoDisc, disc){
+    return prezzoDisc * (1 - disc);
+}
+
+function calcoloPrezzo(value){
+    var priceIngredient = 0;
+
+    for(var i=0; i< ingredients.length; i++){
+        var ingredient = ingredients[i];
+
+        if(ingredient.checked === true){
+            priceIngredient += parseInt(ingredient.value);
+        }
+    }
+    return value + priceIngredient;
+}
+
+
+
 
 
 
 function writePrice(value, target){
-    target.innerHTml = value.toFixed(2);
+    target.innerHTML = value.toFixed(2);
+    
 }
